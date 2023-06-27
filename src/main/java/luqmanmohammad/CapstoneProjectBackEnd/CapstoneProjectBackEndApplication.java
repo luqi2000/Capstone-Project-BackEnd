661fbabaa5a98f9ed19388c453c91a3776e0971c
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-27 11:15:21 +0100

    Initial commit

---
+++ src/main/java/luqmanmohammad/CapstoneProjectBackEnd/CapstoneProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.CapstoneProjectBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luqmanmohammad/CapstoneProjectBackEnd/CapstoneProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.CapstoneProjectBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
